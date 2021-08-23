ffa969d2dd3396b6f6d6c10564c7b0ae46c8cac1
TumanovAlexandr <dkdancer1@gmail.com>
2021-08-23 19:39:51 +0600

    assigment 1 time to practice

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+import "./components/Expenses.css";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <div className="expenses">
+        <ExpenseItem
+          title={Expenses()[0].title}
+          amount={Expenses()[0].amount}
+          date={Expenses()[0].date}
+        />
+        <ExpenseItem
+          title={Expenses()[1].title}
+          amount={Expenses()[1].amount}
+          date={Expenses()[1].date}
+        />
+        <ExpenseItem
+          title={Expenses()[2].title}
+          amount={Expenses()[2].amount}
+          date={Expenses()[2].date}
+        />
+        <ExpenseItem
+          title={Expenses()[3].title}
+          amount={Expenses()[3].amount}
+          date={Expenses()[3].date}
+        />
+      </div>
     </div>
   );
 }
